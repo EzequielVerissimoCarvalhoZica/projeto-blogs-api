b7670541f87e7bf3a06fefd841a57bc501b80ed5
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-03-11 14:31:22 -0300

    feat: migration create table BlogPosts

---
+++ migrations/20220311171044-createBlogPosts.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts',
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      title: Sequelize.STRING,
+      content: Sequelize.STRING,
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      published: Sequelize.DATE,
+      updated: Sequelize.DATE,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
