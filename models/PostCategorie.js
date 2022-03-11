0ab654e9f41769058ab7c6404298f71038da730c
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-03-11 15:29:49 -0300

    feat: post categorie model created

---
+++ models/PostCategorie.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PostCategorie = sequelize.define('PostCategorie', {}, {
+    timestamps: false, tableName: 'PostCategories',
+  });
+
+  PostCategorie.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Categorie, {
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+      through: PostCategorie,
+      as: 'categorys',
+    });
+    models.Categorie.belongsToMany(models.BlogPost, {
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+      through: PostCategorie,
+      as: 'posts',
+    });
+  };
+
+  return PostCategorie;
+};
