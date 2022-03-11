16df2d23cc5a92f098bc71213b909be3b10968d1
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-03-11 08:52:37 -0300

    feat: endpoint skeleton, obrigatory requirements

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
+require('dotenv').config();
+
+const userRouter = require('./middlewares/userRouter');
+const categorieRouter = require('./middlewares/categorieRouter');
+const postRouter = require('./middlewares/postRouter');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/user', userRouter);
+app.use('/categories', categorieRouter);
+app.use('/post', postRouter);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
