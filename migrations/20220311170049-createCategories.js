47855cc6bc4f9ce2b90a99d41e175bbaf80ba6db
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-03-11 14:08:48 -0300

    feat: migration create table Categories

---
+++ migrations/20220311170049-createCategories.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Categories',
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: Sequelize.STRING,
+    });
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Categories');
+
+  }
+};
